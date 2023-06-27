764e42ab8f6d35ce405b60ed17cd42ebdc44fec6
bartuyurdum <bartuyurdum@gmail.com>
2023-06-27 10:19:46 -0700

    First commits

---
+++ src/Car/Toyota.java
@@ -0,0 +1,7 @@
+package Car;
+
+public class Toyota {
+    public void method1(){
+        System.out.println("Hello Batch0");
+    }
+}
---
+++ src/House/SingleFamily.java
@@ -0,0 +1,10 @@
+package House;
+
+public class SingleFamily {
+    public void method1(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("bababababa");
+
+        }
+    }
+}
