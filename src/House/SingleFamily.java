12a180b11ee070fd9527d51f4377f13f432e96d5
bartuyurdum <bartuyurdum@gmail.com>
2023-06-27 10:23:11 -0700

    aaa

--- src/House/SingleFamily.java
+++ src/House/SingleFamily.java
@@ -3,7 +3,7 @@ package House;
 public class SingleFamily {
     public void method1(){
         for (int i = 0; i < 5; i++) {
-            System.out.println("bababababa");
+            System.out.println("bababababaaaa");
 
         }
     }
