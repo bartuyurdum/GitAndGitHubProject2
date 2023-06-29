c2b453e70567b92b22ce27b8cb5be84d644431dc
bartuyurdum <bartuyurdum@gmail.com>
2023-06-28 23:14:25 -0700

    aaaaaa

--- src/House/SingleFamily.java
+++ src/House/SingleFamily.java
@@ -3,7 +3,8 @@ package House;
 public class SingleFamily {
     public void method1(){
         for (int i = 0; i < 5; i++) {
-            System.out.println("this is main project");
+            System.out.println("this is main project" +
+                    "bu da ikinci projem");
 
         }
     }
